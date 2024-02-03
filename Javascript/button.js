cbc7a5151662faed28b2be74c5ba04d2335adda8
Kevin Titus <kxt309@student.bham.ac.uk>
2024-02-03 17:22:26 +0000

    Added the confetti

--- Javascript/button.js
+++ Javascript/button.js
@@ -22,7 +22,26 @@ function yesButton() {
     // Change background color to match gif
     document.body.style.backgroundColor = "rgb(255, 223, 229)";
 
-    // Add count to the message
-    let message = "You clicked no " + count + " times!";
-    document.getElementById("yesMessage").innerHTML = message;
+    //Now this is where shit gets interesting. Basically read the jsdeliver docs and copied this mathy crap: https://www.jsdelivr.com/package/npm/canvas-confetti
+     // Trigger confetti burst
+  var duration = 15 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  var interval = setInterval(function() {
+    var timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    var particleCount = 50 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+  }, 250);
 }
